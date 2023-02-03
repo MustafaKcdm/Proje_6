a96399b2fa84b5033cb19de37e39927dfae6ea7e
MustafaKcdm <mmustafakocaadam@gmail.com>
2023-02-03 20:55:20 +0300

    save  1

--- src/HelperFunctions/HelperFunctions.java
+++ src/HelperFunctions/HelperFunctions.java
@@ -20,6 +20,7 @@ public class HelperFunctions {
             Return tipi: String
             Parametre: String türünden bir city almalı
 
+
         Eğer ki city:
             Address classinin icerisindeki static arrayliste bulunan bir city e eşit değilse
             bir RuntimeException firlatsin ve şu mesaji geçsin:
@@ -33,6 +34,22 @@ public class HelperFunctions {
 
     //---------------------------------------------------------------------------------------
 
+    public static String checkCity(String city){
+
+           boolean cityVarMi=false;
+            try{
+                if (Address.getCityList().contains(city))
+                    cityVarMi=true;
+
+
+
+            }catch (Exception hata){
+                System.out.println("Hata:We can not continue your process because, there is no shipping that location. ");
+
+            }
+        return city;
+
+           }
     //---------------------------------------------------------------------------------------
 
     /*
@@ -48,7 +65,7 @@ public class HelperFunctions {
 
             city nin ilk 3 harfi + zipcode + fullname in baş harfleri
 
-        son olarak ise dönen değer tamamen büyük harde dönmelidir.
+        son olarak ise dönen değer tamamen büyük harfde dönmelidir.
 
         örneğin bir takip kodu: IZM35430CK (Izmir + 35430 + Cem Karaca)
 
@@ -58,9 +75,32 @@ public class HelperFunctions {
 
     //---------------------------------------------------------------------------------------
 
+    public static String generateShippingTrackingNumber(String fullanme, String city, String zipcode){
+        String cityUcHarf = city.substring(0,3).toUpperCase();
+        String isimKodu=null;
+        int kelimeSayisi=1;
+        int boslukIndex = fullanme.indexOf(" ") + 1;
+        int ikinciBoslukIndex = fullanme.lastIndexOf(" ")+1;
+        for (int i = 0; i < fullanme.length(); i++) {
+            if (fullanme.charAt(i)==' '){
+                kelimeSayisi++;
+            }
+        }
+        if (kelimeSayisi==2){
+            isimKodu=fullanme.toUpperCase().charAt(0)+fullanme.toUpperCase().substring(boslukIndex,boslukIndex+1);}
+        else if(kelimeSayisi==3){
+            isimKodu=fullanme.toUpperCase().charAt(0)+fullanme.toUpperCase().substring(boslukIndex,(boslukIndex+1))+
+                    fullanme.toUpperCase().substring(ikinciBoslukIndex,(ikinciBoslukIndex+1));
+        }
+        return cityUcHarf+zipcode+isimKodu;
+
+
+    }
+
     //---------------------------------------------------------------------------------------
 
     /*
+
 
         Bir method oluşturalım ve adına "checkZipCode" diyelim,
             Tipi: static
@@ -74,6 +114,20 @@ public class HelperFunctions {
      */
 
     // Method buraya yazılacak
+
+    public static String checkZipCode(String zipCode){
+        boolean zipCodeLenght;
+        try {
+            if (zipCode.length()==5)
+                zipCodeLenght=true;
+
+        }catch (Exception hata){
+            System.out.println("Hata: Please enter valid Zip Code");
+
+        }
+        return zipCode;
+
+    }
 
     //---------------------------------------------------------------------------------------
 
--- src/Models/User.java
+++ src/Models/User.java
@@ -18,9 +18,52 @@ public class User {
         Attribute lar burada tanımlanıcak -->
 
      */
+   private String fullName;
+   private String city;
+   private String address;
+   private String zipCode;
+   private double myPrice;
+   private String shippingTrackingNumber;
 
+    public User(String fullName, String city, String address, String zipCode) {
+        setFullName(fullName);
+        setCity(city);
+        HelperFunctions.checkCity(city);
+        HelperFunctions.checkZipCode(zipCode);
+    }
 
-    /*
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+/*
 
         Bu class bir constructor içermeli ve gelen
             ** fullname, address, city, zipcode değerlerini parametre olarak almalı.**
@@ -32,7 +75,9 @@ public class User {
 
         // Constructor burada tanımlanacak
 
+
      */
+
 
     /*
 
